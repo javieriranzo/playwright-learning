7a932b70c906d0507e71e1c35be5b12aba32611b
Javier Iranzo <jiranzo@iti.es>
2024-02-15 13:32:07 +0100

    Primer test con playwright + java

--- playwright-learning-java/src/main/java/org/example/Main.java
+++ playwright-learning-java/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.webkit().launch();
+            Page page = browser.newPage();
+            page.navigate("https://playwright.dev/");
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+        }
     }
 }
